5b18ddb5cac6c6d27dac20eb9e2dd129b067e6b1
rcollins <robertcollins130@gmail.com>
2023-06-03 13:13:01 -0700

    Initial Commit

---
+++ aa201b_ray_trace.m
@@ -0,0 +1,29 @@
+%%
+% FINAL PROJECT: AIRCRAFT NOISE RAY TRACING
+% AA201B STANFORD SPRING 2023
+% ROBERT COLLINS
+
+%% SETUP
+clear; close all;
+
+%% PARAMETERS
+
+% define environment
+
+% z coords
+% x coords
+
+% temperature profile
+% pressure profile
+% density profile
+
+% source location
+% receiver location
+
+% source level 
+
+% define sim parameters
+
+% define plotting parameters
+
+%% RAY TRACE
