330f4b5b4962b397a0e98b08b4f6ad426ca12b79
rcollins <robertcollins130@gmail.com>
2023-06-11 05:00:37 -0700

    parallelize 3d ray tracer

--- aa201b_ray_trace_3d.m
+++ aa201b_ray_trace_3d.m
@@ -16,6 +16,20 @@ x_range = [-1000, 1000]; %m
 y_range = [-1000, 1000];
 t_max = 300; %s
 
+% 
+n_rays_theta = 8;
+n_rays_phi = 5;
+
+min_theta = 17/32*pi;
+max_theta = 3/4*pi;
+min_phi = 0;
+max_phi = pi;
+
+use_rk4 = 1;
+
+% source location
+r_0 = [0,0,5000];
+
 %% PHYSICAL PARAMETERS
 % Physical Constants
 %   Schafer and Vorlander
@@ -45,6 +59,8 @@ v_s = 0.05;
 z = linspace(z_range(1), z_range(2), (diff(z_range))/dz + 1)';
 t = linspace(0, t_max, (t_max)/dt + 1)';
 
+round2zlim = @(ii_z)(min(max(ii_z,1), size(z,1)));
+
 % temperature profile
 T = T_0 - L_b * z;
 
@@ -56,8 +72,8 @@ vx(z<z_0) = 0;
 v = [vx, zeros(size(vx)), zeros(size(vx))]; % x, y, z
 
 % sound speed profile
-%c = sqrt(gamma * R * T);
-c = -sawtooth(2*pi*z/max(z), 1/2)*10 + 350;
+c = sqrt(gamma * R * T);
+%c = -sawtooth(2*pi*z/max(z), 1/2)*10 + 350;
 % c = 343*ones(size(z));
 
 % compute derivatives of profiles
@@ -83,72 +99,45 @@ xlabel('Horizontal Wind Velocity, m/s')
 ylabel('Altitude, m')
 
 %% RAY TRACE
-
-% 
-n_rays_theta = 8;
-n_rays_phi = 1;
-
-min_theta = 17/32*pi;
-max_theta = 3/4*pi;
-min_phi = 0;
-max_phi = 0;
-
 % ray angle rel2 positive x
 la_theta = linspace(min_theta, max_theta, n_rays_theta);
-la_phi = linspace(min_phi, max_phi, n_rays_phi+1);
-la_phi = la_phi(1:end-1);
+la_phi = linspace(min_phi, max_phi, n_rays_phi);
 
-r_0 = [0,0,5000];
-
-cmap = parula(n_rays_theta);
-figure(2); clf; hold on
-ax = gca();
-axis equal
-view([1 1 1])
-xlabel('x')
-ylabel('y')
-zlabel('z')
-colormap(cmap)
-cb = colorbar();
-dtheta = (max_theta - min_theta)/(n_rays_theta-1);
-clim([min_theta-dtheta/2, max_theta+dtheta/2])
-cb.Ticks = la_theta;
-cb.TickLabels = compose("%0.2f \\pi", la_theta/pi);
-ylabel(cb, 'Ray Launch Elevation')
-r = zeros(size(t,1), 3, n_rays_theta, n_rays_phi);
-r(1,:,:,:) = repmat(r_0, 1, 1, n_rays_theta, n_rays_phi);
-
-% plot_array = cell(n_rays_theta, n_rays_phi);
-% for ii_phi = 1:n_rays_phi
-% for ii_theta = 1:n_rays_theta
-%     plot_array{ii_theta, ii_phi} = plot3( ...
-%         r(:,1,ii_theta,ii_phi), ...
-%         r(:,2,ii_theta,ii_phi), ...
-%         r(:,3,ii_theta,ii_phi), ...
-%         'Color', cmap(ii_theta, :) ...
-%     );
-% end
-% end
-
-for ii_phi = 1:n_rays_phi
+n_rays = n_rays_theta * n_rays_phi;
+la = zeros(n_rays_theta*n_rays_phi, 4);
+ii_ray = 1;
 for ii_theta = 1:n_rays_theta
+    for ii_phi = 1:n_rays_phi
+        la(ii_ray,:) = [la_theta(ii_theta), la_phi(ii_phi), ii_theta, ii_phi];
+        ii_ray = ii_ray +1;
+    end
+end
 
+r = zeros(size(t,1), 3, n_rays);
+max_ii_t = repmat(size(t,1), n_rays,1);
+%r(1,:,:,:) = repmat(r_0, 1, 1, n_rays_theta, n_rays_phi);
+
+tic 
+parfor ii_ray = 1:n_rays    
+    this_r = zeros(size(t, 1), 3);
+    this_r(1,:) = r_0;
     % initialize ray vector
     % r = zeros(size(t, 1), 3);
     % n = zeros(size(t, 1), 3);
-
+    
     % initial ray normal 
     n = [
-        sin(la_theta(ii_theta))*cos(la_phi(ii_phi)) ...
-        sin(la_theta(ii_theta))*sin(la_phi(ii_phi)) ...
-        cos(la_theta(ii_theta))
+        sin(la(ii_ray, 1)) * cos(la(ii_ray, 2)) ...
+        sin(la(ii_ray, 1)) * sin(la(ii_ray, 2)) ...
+        cos(la(ii_ray, 1))
     ]; % x, y ,z
 
     % compute starting ii_z
-    [~, ii_z] = min(abs(z-r(1, 3)));
+    % TODO: possibly move outside
+    [~, ii_z0] = min(abs(z-this_r(1, 3)));
 
     % compute initial slowness vector
-    s = n ./ (c(ii_z) + n * v(ii_z,:)');
+    s = n ./ (c(ii_z0) + n * v(ii_z0,:)');
     
     % compute slowness perp and omega vec
     s_perp = s(1:2);
@@ -165,62 +154,71 @@ for ii_theta = 1:n_rays_theta
         c(ii_z).^2 ./ omega(ii_z) * s_z;
         -omega(ii_z)./c(ii_z) * c_dz(ii_z) - s_perp * v_dz(ii_z,1:2)';
     ];
-
-    max_ii_t = size(t,1);
-
+    
+    ii_z1 = ii_z0;
     % iterate thru time
     for ii_t = 1:size(t,1)-1
-        % get current ii_z 
-        [~, ii_z1] = min(abs(z-r(ii_t, 3, ii_theta, ii_phi)));
-        
-        if ii_z1 == 1 || ii_z1 == size(z, 1)
-            max_ii_t = ii_t;
+        if ii_z1 <= 1 || ii_z1 >= size(z, 1)
+            max_ii_t(ii_ray) = ii_t;
             break
         end
-
-        % advance values (euler method)
-        r(ii_t+1, :, ii_theta, ii_phi) = r(ii_t,:, ii_theta, ii_phi) + ...
-            (c(ii_z1).^2 / omega(ii_z1) .* [s_perp, s_z] + v(ii_z1,:)) .* dt;
-        s_z = s_z + sz_dt(ii_z1) * dt;
-
-        % advance values (RK4)
-        z1 = z(ii_z1);
-        a1 = f(ii_z1, s_z);
-
-        z2 = z1 + dt/2 * a1(3);
-        %[~, ii_z2] = min(abs(z-z2));
-        ii_z2 = abs(ii_z1 + round((z2-z1)/dz));
-        a2 = f(ii_z2, s_z + dt/2 * a1(4));
-
-        z3 = z1 + dt/2 * a2(3);
-        % [~, ii_z3] = min(abs(z-z3));
-        ii_z3 = abs(ii_z1 + round((z3-z1)/dz));
-        a3 = f(ii_z3, s_z + dt/2 * a2(4));
-
-        z4 = z1 + dt * a3(3);
-        % [~, ii_z4] = min(abs(z-z4));
-        ii_z4 = abs(ii_z1 + round((z4-z1)/dz));
-        a4 = f(ii_z4, s_z + dt * a3(4));
-
-        dx = dt/6 * (a1 + 2*a2 + 2*a3 + a4);
-
-        r(ii_t+1, :, ii_theta, ii_phi) = r(ii_t, :, ii_theta, ii_phi) + dx(1:3)';
-        s_z = s_z + dx(4);
-
-        % update plot
-        % plot_array{ii_theta, ii_phi}.XData(ii_t) = r(ii_t, 1, ii_theta, ii_phi);
-        % plot_array{ii_theta, ii_phi}.YData(ii_t) = r(ii_t, 2, ii_theta, ii_phi);
-        % plot_array{ii_theta, ii_phi}.ZData(ii_t) = r(ii_t, 3, ii_theta, ii_phi);
-        % pause(0.0001)
-    end
+        
+        if use_rk4
+            % advance values (RK4)
+            z1 = this_r(ii_t, 3);
+            a1 = f(ii_z1, s_z);
     
-    plot3(ax, ...
-        r(1:max_ii_t, 1, ii_theta, ii_phi),...
-        r(1:max_ii_t, 2, ii_theta, ii_phi),...
-        r(1:max_ii_t, 3, ii_theta, ii_phi),...
-        'Color', cmap(ii_theta,:)...
-        );
-    pause(0.01)
+            z2 = z1 + dt/2 * a1(3);
+            ii_z2 = round2zlim(ii_z1 + round((z2-z1)/dz));
+            a2 = f(ii_z2, s_z + dt/2 * a1(4));
+    
+            z3 = z1 + dt/2 * a2(3);
+            ii_z3 = round2zlim(ii_z1 + round((z3-z1)/dz));
+            a3 = f(ii_z3, s_z + dt/2 * a2(4));
+    
+            z4 = z1 + dt * a3(3);
+            ii_z4 = round2zlim(ii_z1 + round((z4-z1)/dz));
+            a4 = f(ii_z4, s_z + dt * a3(4));
+    
+            dx = dt/6 * (a1 + 2*a2 + 2*a3 + a4);
+    
+            this_r(ii_t+1, :) = this_r(ii_t, :) + dx(1:3)';
+            s_z = s_z + dx(4);
+        else
+            % advance values (euler method)
+            this_r(ii_t+1, :) = this_r(ii_t,:) + ...
+                (c(ii_z1).^2 / omega(ii_z1) .* [s_perp, s_z] + v(ii_z1,:)) .* dt;
+            s_z = s_z + sz_dt(ii_z1) * dt;
+        end
+        
+        ii_z1 = round2zlim(ii_z0 + round((this_r(ii_t+1, 3)-r_0(3))/dz));
+    end
+    r(:,:,ii_ray) = this_r;
 end
-end
+toc
 
+cmap = parula(n_rays_theta);
+figure(2); clf; hold on
+ax = gca();
+axis equal
+view([1 1 1])
+xlabel('x')
+ylabel('y')
+zlabel('z')
+colormap(cmap)
+cb = colorbar();
+dtheta = (max_theta - min_theta)/(n_rays_theta-1);
+clim([min_theta-dtheta/2, max_theta+dtheta/2])
+cb.Ticks = la_theta;
+cb.TickLabels = compose("%0.2f \\pi", la_theta/pi);
+ylabel(cb, 'Ray Launch Elevation')
+
+for ii_ray = 1:n_rays
+    plot3(ax, ...
+            r(1:max_ii_t(ii_ray), 1, ii_ray),...
+            r(1:max_ii_t(ii_ray), 2, ii_ray),...
+            r(1:max_ii_t(ii_ray), 3, ii_ray),...
+            'Color', cmap(la(ii_ray,3),:)...
+            );
+        pause(0.01)
+end
