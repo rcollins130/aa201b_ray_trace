7e6e9d5c2e92bd67b93bf22ceefede8d38dc503f
rcollins <robertcollins130@gmail.com>
2023-06-11 11:54:50 -0700

    clean up 3d ray tracer

--- aa201b_ray_trace_3d.m
+++ aa201b_ray_trace_3d.m
@@ -8,82 +8,74 @@ clear;
 % close all;
 
 %% SIMULATION PARAMETERS
-dz = 1; % m
+% time steps
 dt = 0.01; %s
-
-z_range = [0, 3500]; %m
-x_range = [-5000, 5000]; %m
-y_range = [-5000, 5000];
 t_max = 600; %s
+nt = t_max / dt + 1;
 
-% 
-n_rays_ele = 50;
-n_rays_azi = 1;
+% profiles
+dz = 0.1; % m
+z_range = [0, 3500]; %m
 
-min_ele_deg = -90;
-max_ele_deg = 90;
-
-min_ele = deg2rad(min_ele_deg);
-max_ele = deg2rad(max_ele_deg);
-min_azi = 0;
-max_azi = 0;
-
-use_rk4 = 1;
+% rays
+n_rays_ele = 25;
+n_rays_azi = 4;
+min_ele = deg2rad(-90);
+max_ele = deg2rad(90);
+min_azi = deg2rad(-180);
+max_azi = deg2rad(90);
 
 % source location
 r_0 = [0,0,500];
 
+% wind azimuth
+wind_azi = deg2rad(0);
+
 %% PHYSICAL PARAMETERS
-% Physical Constants
-%   Schafer and Vorlander
-% temperature gradient
 T_0 = 288.15; % Reference sea level temperature, K
 L_b = 0.0065; % Temperature lapse rate, K/m
-
 gamma= 1.4; % adiabatic index
 R = 287.058; % molar gas constant, dry air, m2/s2
-
-% velocity gradient
-% https://en.wikipedia.org/wiki/Log_wind_profile
-% https://en.wikipedia.org/wiki/Wind_profile_power_law
-
-% Karman Constant
-kar = 0.40; 
-
-% Surface roughness, m 
-% https://en.wikipedia.org/wiki/Roughness_length
-z_0 = 0.1; 
-
-% Friction velocity, m/s
-%   Schafer and Vorlander
-v_s = 0.6;
+kar = 0.40; % Karman Constant
+z_0 = 0.1; % Surface roughness, m 
+v_s = 0.6; % Friction velocity, m/s
 
 %% DEFINE ENVIRONMENT
+% height profile
 z = linspace(z_range(1), z_range(2), (diff(z_range))/dz + 1)';
-t = linspace(0, t_max, (t_max)/dt + 1)';
-
-round2zlim = @(ii_z)(min(max(ii_z,1), size(z,1)));
 
 % temperature profile
 T = T_0 - L_b * z;
 
 % velocity profile
 % TODO: ADD POWER LAW ABOVE ~1000M?
-vx= v_s/kar .* log(z/z_0);
-vx(z<z_0) = 0;
-
-v = [vx, zeros(size(vx))]; % x, y
+v_mag= v_s/kar .* log(z/z_0);
+v = -v_mag * [cos(wind_azi), sin(wind_azi)]; % x, y
+v(z<z_0, :) = 0;
 
 % sound speed profile
 c = sqrt(gamma * R * T);
 % c = -sawtooth(2*pi*z/max(z), 1/2)*10 + 350;
 % c = 343*ones(size(z));
 
-% compute derivatives of profiles
-v_dz = [diff(v,1,1)/dz; zeros(1,2)];
-c_dz = [diff(c)/dz; 0];
 
-%% INITIAL PLOTTING
+%% RAY TRACE
+[la, ele_idx, ~] = launch_angles(...
+    min_ele, max_ele, n_rays_ele, ...
+    min_azi, max_azi, n_rays_azi);
+
+r = zeros(nt, 3, size(la,1));
+max_ii_t = repmat(nt, size(la,1),1);
+
+tic 
+for ii_ray = 1:size(la,1)    
+    [r(:,:,ii_ray), max_ii_t(ii_ray)]= raytrace_rac( ...
+        dt, nt, dz, r_0, la(ii_ray,1), la(ii_ray,2), z, v, c ...
+        );
+end
+toc
+
+%% PLOTTING
 % Plot environmental profile
 figure(1); clf;
 subplot(1,4,1)
@@ -97,37 +89,11 @@ xlabel('Sound Speed, m/s')
 ylabel('Altitude, m')
 
 subplot(1,4,4)
-plot(vx,z)
-xlabel('Horizontal Wind Velocity, m/s')
+plot(vecnorm(v,2,2),z)
+xlabel('Wind Velocity, m/s')
 ylabel('Altitude, m')
 
-%% RAY TRACE
-% ray angle rel2 positive x
-la_ele = linspace(min_ele, max_ele, n_rays_ele);
-la_azi = linspace(min_azi, max_azi, n_rays_azi);
-
-n_rays = n_rays_ele * n_rays_azi;
-la = zeros(n_rays_ele*n_rays_azi, 4);
-ii_ray = 1;
-for ii_theta = 1:n_rays_ele
-    for ii_phi = 1:n_rays_azi
-        la(ii_ray,:) = [la_ele(ii_theta), la_azi(ii_phi), ii_theta, ii_phi];
-        ii_ray = ii_ray +1;
-    end
-end
-
-r = zeros(size(t,1), 3, n_rays);
-max_ii_t = repmat(size(t,1), n_rays,1);
-%r(1,:,:,:) = repmat(r_0, 1, 1, n_rays_theta, n_rays_phi);
-
-tic 
-for ii_ray = 1:n_rays    
-    [r(:,:,ii_ray), max_ii_t(ii_ray)]= raytrace_rac( ...
-        dt, size(t,1), dz, r_0, la(ii_ray,1), la(ii_ray,2), z, v, c ...
-        );
-end
-toc
-
+% Plot Rays
 cmap = parula(n_rays_ele);
 figure(2); clf; hold on
 ax = gca();
@@ -144,12 +110,12 @@ clim([min_ele, max_ele])
 cb.TickLabels = compose("%0.2fº", rad2deg(cb.Ticks));
 ylabel(cb, 'Ray Launch Elevation')
 
-for ii_ray = 1:n_rays
+for ii_ray = 1:size(la,1)
     plot3(ax, ...
             r(1:max_ii_t(ii_ray), 1, ii_ray),...
             r(1:max_ii_t(ii_ray), 2, ii_ray),...
             abs(r(1:max_ii_t(ii_ray), 3, ii_ray)),...
-            'Color', cmap(la(ii_ray,3),:)...
+            'Color', cmap(ele_idx(ii_ray),:)...
             );
         pause(0.01)
 end
---
+++ launch_angles.m
@@ -0,0 +1,24 @@
+function [la, ele_idx, azi_idx] = launch_angles(min_ele,max_ele, n_rays_ele, min_azi,max_azi,n_rays_azi)
+%LAUNCH_ANGLES Summary of this function goes here
+%   Detailed explanation goes here
+
+la_ele = linspace(min_ele, max_ele, n_rays_ele);
+la_azi = linspace(min_azi, max_azi, n_rays_azi);
+
+la = zeros(n_rays_ele*n_rays_azi, 2);
+ele_idx = zeros(n_rays_ele*n_rays_azi, 1);
+azi_idx = zeros(n_rays_ele*n_rays_azi, 1);
+
+ii_ray = 1;
+for ii_theta = 1:n_rays_ele
+    for ii_phi = 1:n_rays_azi
+        la(ii_ray,:) = [la_ele(ii_theta), la_azi(ii_phi)];
+        ele_idx(ii_ray) = ii_theta;
+        azi_idx(ii_ray) = ii_phi;
+        ii_ray = ii_ray +1;
+    end
+end
+
+end
+
+ 
